826444982f568c8a326b54d90cd9a6172321ab64
Hycarus <mirkomarasco08@gmail.com>
2023-11-07 13:45:33 +0100

    added function for generate mail from api

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ createApp({
 // contiene tutti i dati / le variabili 
 data(){
     return {
+        apiURL: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        emailArray: [],
     }
 },
 // contiene le funzioni e i metodi
 methods: {
 },
 mounted() {
+    for(let i = 0; i < 10; i++){
+        axios.get(this.apiURL).then((resp) => {
+            this.emailArray.push(resp.data.response);
+            console.log(resp.data.response);
+        })
+    }
 }
 }).mount('#app')
